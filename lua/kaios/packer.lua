1a8a38e00b1230320c88433c492797f2d6e59897
NeoKaios <mateo.dugas@student-cs.fr>
2022-09-13 22:45:48 +0200

    ensure packer install and readme

--- lua/kaios/packer.lua
+++ lua/kaios/packer.lua
@@ -1,5 +1,15 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then 
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 require('packer').startup(function(use)
   -- Packer can manage itself
@@ -52,6 +62,12 @@ require('packer').startup(function(use)
 
   -- Visuals
   use 'folke/tokyonight.nvim'
+
+  if packer_bootstrap then
+	  require'packer'.sync()
+  end
+
+
 end)
 
 
