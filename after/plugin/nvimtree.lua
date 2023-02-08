17e65ec31899ca8364544c0f574ade95e492c5e8
NeoKaios <mateo.dugas@student-cs.fr>
2023-02-08 19:27:18 +0000

    Fixed directory opening with cd and changed space-fd map

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -16,16 +16,24 @@ require('nvim-tree').setup({
 })
 
 -- focus and find file
-vim.keymap.set("n", "<leader>fd",
+vim.keymap.set("n", "<leader>fD",
 function()
     require('nvim-tree').toggle(false, false)
     require('nvim-tree').toggle(true, false)
 end
 )
+-- focus and dont find
+vim.keymap.set("n", "<leader>fd", function() require('nvim-tree').focus() end)
+-- toggle without focusing
 vim.keymap.set("n", "<leader>ft", function() require('nvim-tree').toggle(false, true) end)
 
 -- open tree on VimEnter
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function()
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function(data)
+    local directory = vim.fn.isdirectory(data.file) == 1
+    -- change to the directory
+    if directory then
+        vim.cmd.cd(data.file)
+    end
     require("nvim-tree").toggle(true, false)
 end
  })
