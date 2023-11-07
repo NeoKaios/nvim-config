2740ce7361dd858978f018ac70c4aa33cb75db46
NeoKaios <mdgscode@mailo.com>
2023-11-07 20:02:26 +0100

    fix: fixed and updated conf for neovide 0.12

--- lua/kaios/neovide.lua
+++ lua/kaios/neovide.lua
@@ -1,7 +1,9 @@
 if (vim.g.neovide) then
     local g = vim.g
-    vim.opt.guifont = { 'FiraCode Nerd Font Mono', ':h11' }
+    vim.opt.guifont = 'FiraCode Nerd Font:h8'
     g.neovide_remember_window_size = true
+    g.neovide_scroll_animation_length = 0.15
+    g.neovide_hide_mouse_when_typing = true
     g.reovide_refresh_rate_idel = 0
     g.neovide_cursor_vfx_mode = "railgun"
 end
